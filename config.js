326a1ed1dac115bb7a72f1e9b28d0ac3a627b896
Dave Cassel <dmcassel@gmail.com>
2017-03-16 12:45:27 -0400

    updating port to reflect changed port for Geophoto

--- config.js
+++ config.js
@@ -1,35 +1,35 @@
 module.exports = {
   admin: {
     host: 'localhost',
-    port: 5003,
+    port: 8010,
     database: 'geophoto-content',
     user: 'admin',
     password: 'admin'
   },
   nobody: {
     host: 'localhost',
-    port: 5003,
+    port: 8010,
     database: 'geophoto-content',
     user: 'hon-nobody',
     password: 'nobodyPass'
   },
   reader: {
     host: 'localhost',
-    port: 5003,
+    port: 8010,
     database: 'geophoto-content',
     user: 'hon-reader',
     password: 'readerPass'
   },
   writer: {
     host: 'localhost',
-    port: 5003,
+    port: 8010,
     database: 'geophoto-content',
     user: 'hon-writer',
     password: 'writerPass'
   },
   restAdmin: {
     host: 'localhost',
-    port: 5003,
+    port: 8010,
     database: 'geophoto-content',
     user: 'hon-admin',
     password: 'adminPass'
